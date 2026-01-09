74209adbbce58ea7d78160998ddd92eafc633c65
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-01-10 02:19:46 +1300

    Add penguin loading animation as LoadingScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import Card from "./components/Card/Card.jsx";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen.jsx";
 import Introduction from "./components/Introduction/Introduction.jsx";
+import React, {useEffect, useState} from "react";
 
 function App() {
 
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 6000);
+
+        return () => clearTimeout(timer);
+    }, [])
+
+    const content = (
+        <>
+            <Introduction></Introduction>
+        </>
+    );
+
   return (
     <>
-        <Introduction></Introduction>
+        {isLoading ? <LoadingScreen></LoadingScreen> : content}
     </>
 
   )
