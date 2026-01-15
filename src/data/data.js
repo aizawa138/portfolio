b667786e917df27cf15b4d02d0e7c1122f402fe2
Daigo Kitagawa <daigokitagawa1019@gmail.com>
2026-01-15 23:32:42 +1300

    Change directory name from project.js to data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,11 @@
 import fishImage from "../assets/images/IMG_1099.jpg";
 
 export const CARD_DATA = [
+        {
+            image: fishImage,
+            title: "Project 5",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit est deleniti veritatis, qui adipisci ab."
+        },
         {   
             image: fishImage,
             title: "Project 1",
@@ -19,11 +24,6 @@ export const CARD_DATA = [
         {   
             image: fishImage,
             title: "Project 4",
-            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit est deleniti veritatis, qui adipisci ab."
-        },
-        {   
-            image: fishImage,
-            title: "Project 5",
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit est deleniti veritatis, qui adipisci ab."
         },
     ];
